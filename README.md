f3d1938e925746f5a35425f014ec21ae4a136ec3
Michael Krotscheck <krotscheck@gmail.com>
2016-04-20 12:24:55 -0700

    Project initial commit
    The initial commit of this repository adds an afterAll hook which
    checks for the current active sync sequence ID. If this ID matches
    the latest ID from the registry, it will exit the process cleanly.
    
    Exiting in this fashion is required, so that we can push the AFS
    filesystem to the master nodes when it is in known a consistent state.

---
+++ README.md
@@ -0,0 +1,37 @@
+# openstack-registry-hooks
+This project provides hooks necessary for the function of OpenStack's 
+AFS-based NPM mirrors. It is maintained by OpenStack's Infrastructure team.
+
+## Provided Hooks
+
+#### afterAll
+After each package is processed, the script will check the overall 
+synchronization status of the mirror. If it detects that the mirror is up to 
+date, it will exit the process.
+
+This is done to ensure that the mirror is in a consistent state, before 
+synchronizing the slave disk to the AFS Master. Resuming the process is 
+handled via cron.
+
+## QuickStart
+
+	#!/bin/bash
+	
+	# Install the registry and the hook module.
+	npm install -g registry-static openstack-registry-hooks
+	
+	# Run the registry script
+	registry-static -d my.registry.com -o /var/www/registry \
+		--hooks openstack-registry-hooks
+
+## Some useful development commands
+
+* `npm test` - Run all the tests, with coverage.
+* `npm run lint` - Perform a linting check.
+
+## Project Resources
+
+  - [Source code](https://git.openstack.org/cgit/openstack/js-openstack-registry-hooks)
+  - [How to contribute to OpenStack](http://docs.openstack.org/infra/manual/developers.html)
+  - [Code review workflow](http://docs.openstack.org/infra/manual/developers.html#development-workflow)
+  - IRC: \#openstack-infra on \#freenode
---
+++ spec/index.js
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2016 Hewlett Packard Enterprise Development Company, LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+(function () {
+  'use strict';
+
+  describe('openstack-registry-hooks', function () {
+    var oldProc = process.exit;
+    var procSpy;
+    var boundScope = {
+      log: {
+        info: function () {
+        }
+      }
+    };
+
+    beforeEach(function () {
+      procSpy = jasmine.createSpy('exit');
+      process.exit = procSpy;
+    });
+
+    afterEach(function () {
+      process.exit = oldProc;
+    });
+
+    it('should implement the entire hook interface', function () {
+      var hooks = require('../index');
+
+      expect(hooks.beforeAll).toBeDefined();
+      expect(hooks.afterAll).toBeDefined();
+      expect(hooks.globalIndexJson).toBeDefined();
+      expect(hooks.indexJson).toBeDefined();
+      expect(hooks.versionJson).toBeDefined();
+      expect(hooks.tarball).toBeDefined();
+      expect(hooks.afterTarball).toBeDefined();
+      expect(hooks.startup).toBeDefined();
+      expect(hooks.shasumCheck).toBeDefined();
+    });
+
+    it('should noop on most methods', function () {
+      var hooks = require('../index');
+
+      var methods = ['beforeAll', 'globalIndexJson', 'indexJson',
+                     'versionJson', 'tarball', 'afterTarball', 'startup',
+                     'shasumCheck'];
+      methods.forEach(function (methodName) {
+        var data = {};
+        var cb = jasmine.createSpy('spy');
+        hooks[methodName](data, cb);
+
+        expect(data).toEqual({});
+        expect(cb).toHaveBeenCalledWith(null, true);
+      });
+    });
+
+    it('should noop on afterAll if the sequences do not match',
+      function () {
+        var hooks = require('../index');
+        var cb = jasmine.createSpy('spy');
+        var data = {seq: 2, latestSeq: 20};
+
+        hooks.afterAll.call(boundScope, data, cb);
+
+        expect(procSpy).not.toHaveBeenCalled();
+        expect(cb).toHaveBeenCalledWith(null, true);
+      });
+
+    it('should not error if sequences are not defined',
+      function () {
+        var hooks = require('../index');
+        var cb = jasmine.createSpy('spy');
+        var data = {};
+
+        hooks.afterAll.call(boundScope, data, cb);
+
+        expect(procSpy).not.toHaveBeenCalled();
+        expect(cb).toHaveBeenCalledWith(null, true);
+      });
+
+    it('should process.exit() if sequences match',
+      function () {
+        var hooks = require('../index');
+        var cb = jasmine.createSpy('spy');
+        var data = {seq: 20, latestSeq: 20};
+
+        hooks.afterAll.call(boundScope, data, cb);
+
+        expect(procSpy).toHaveBeenCalledWith(0);
+      });
+
+  });
+})();
